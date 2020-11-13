300d4c3dd8d58e6eea1cdf97586dc6fdef96a24a
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-13 11:42:23 +0100

    Setup Clever Cloud config files

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,22 @@
 class DosesController < ApplicationController
   def new
-    @doses = Dose.new
-    # @ingredients = Ingredient.all
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     # @cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+
   end
 
   def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id)
   end
 end
